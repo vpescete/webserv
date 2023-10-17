bd16a4b54ddaaafe8f5ae1f3e499281447aa94f6
Valerio Pescetelli <vpescetelli@MBP-Summer5.local>
2023-10-17 17:57:39 +0200

    nothing special

--- include/webserv.h
+++ include/webserv.h
@@ -5,7 +5,7 @@
 // ANSI Colors
 # define RED "\e[0;31m"
 # define GREEN "\e[0;32m"
-# define YELLOW "\e[0;33m"
+# define YELLOW "\e[93;5;226m"
 # define BLUE "\e[0;34m"
 # define MAGENTA "\e[0;35m"
 # define CYAN "\e[0;36m"
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,14 +1,25 @@
 #include "webserv.h"
 
+bool	running = 1;
+
+static void signal_handler(int i) {
+	if (i == SIGINT)
+		running = 0;
+	std::cout << std::endl << YELLOW << "Stopping server..." << RESET << std::endl;
+}
+
 int	main(int ac, char *av[]) {
 
 	if (ac != 2) {
 		std::cout << RED << "ERROR: wrong number of arguments" << RESET <<std::endl;
 		return EXIT_FAILURE;
 	}
+	signal(SIGINT, signal_handler);
 	ServerConf confFile(av[1]);
 	// std::cout << confFile.getHost() << std::endl;
 	// std::cout << confFile.getPort() << std::endl;
 	Server	svr(confFile);
+	while (running)
+		;
 }
 
