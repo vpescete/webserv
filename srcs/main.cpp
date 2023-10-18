9d25d9f281b48f371ecb4c16d09ba98ff6e5fbec
Giacomo <giacominho3@DESKTOP-CCUI3S4.localdomain>
2023-10-18 10:29:24 +0200

    creating gifulvi-branch

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,14 +1,81 @@
-#include "webserv.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
-int	main(int ac, char *av[]) {
+#define PORT 8080
 
-	if (ac != 2) {
-		std::cout << RED << "ERROR: wrong number of arguments" << RESET <<std::endl;
-		return EXIT_FAILURE;
-	}
-	ServerConf confFile(av[1]);
-	// std::cout << confFile.getHost() << std::endl;
-	// std::cout << confFile.getPort() << std::endl;
-	Server	svr(confFile);
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    FILE *html_file = fopen("../www/tmp/index.html", "r");
+    if (html_file == NULL)
+	{
+        perror("Impossibile aprire il file 'index.html'");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(html_file, 0, SEEK_END);
+    long file_size = ftell(html_file);
+    fseek(html_file, 0, SEEK_SET);
+
+    char *html_content = (char *)malloc(file_size);
+    fread(html_content, 1, file_size, html_file);
+    fclose(html_file);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen) < 0))
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+
+        char buffer[30000] = {0};
+        read(new_socket, buffer, 30000);
+        printf("%s\n", buffer);
+        
+        char response[30000] = {0};
+        snprintf(response, sizeof(response),
+                 "HTTP/1.1 200 OK\r\n"
+                 "Content-Type: text/html\r\n"
+                 "Content-Length: %ld\r\n\r\n%s",
+                 file_size, html_content);
+
+        write(new_socket, response, strlen(response));
+        printf("------------------HTML content sent-------------------");
+        close(new_socket);
+    }
+
+    free(html_content);
+    return 0;
 }
-
