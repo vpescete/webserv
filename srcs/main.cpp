de3a0bc6671ce21049978abf7a1e3eab16317486
Valerio Pescetelli <vpescetelli@MBP-Summer5.local>
2023-10-18 09:23:32 +0200

    Still try to understand what im doing..

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -19,7 +19,41 @@ int	main(int ac, char *av[]) {
 	// std::cout << confFile.getHost() << std::endl;
 	// std::cout << confFile.getPort() << std::endl;
 	Server	svr(confFile);
-	while (running)
-		;
+
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Configura l'indirizzo del server
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(8080); // Porta 8080
+
+    // Collega il socket all'indirizzo del server
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    // Mette il server in ascolto su localhost
+    listen(serverSocket, 5);
+
+    std::cout << "Server in ascolto su localhost:8080" << std::endl;
+	int clientSocket = accept(serverSocket, NULL, NULL);
+	while (running) {
+
+        // Leggi e invia il file index.html
+        std::ifstream file("/Users/vpescetelli/webserv/www/index.html");
+        if (file.is_open()) {
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            std::string content = buffer.str();
+            std::string response = "HTTP/1.1 200 OK\r\nContent-Length: " + std::to_string(content.length()) + "\r\n\r\n" + content;
+            send(clientSocket, response.c_str(), response.length(), 0);
+        } else {
+            std::string response = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+            send(clientSocket, response.c_str(), response.length(), 0);
+        }
+
+        // Chiudi la connessione con il client
+    	close(clientSocket);
+	}
+	close(serverSocket);
 }
 
