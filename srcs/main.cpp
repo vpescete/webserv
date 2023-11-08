e102d00f9316a4fd0d331b9688276b31eb693298
Valerio Pescetelli <vpescetelli@MBP-Summer5.local>
2023-11-08 18:33:52 +0100

    Request, Client and Parser re working. Missing Response and cgi

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -53,7 +53,6 @@ int	main(int ac, char *av[]) {
 	std::vector<Server *> srvs;
 	srvs = startServer(confFile.getMapConfig());
 	RequestHandler req;
-	// char * bufferino = (char *)malloc(100000);
 	Clients client;
 	struct kevent events[MAXEVENTS];
 	ssize_t addrlen = sizeof(sockaddr);
@@ -66,31 +65,23 @@ int	main(int ac, char *av[]) {
 		for (int i = 0; i < numEvents; i++) {
 			int	index = getRightSocketFd(srvs, events[i].ident);
 			if (index != -1) {
-				// std::cout << CYAN << "Found server with index: " << index << RESET << std::endl;
 				connect = accept(events[i].ident, (struct sockaddr *)(*srvs[index]).getServerAddress(), (socklen_t*)&addrlen);
-				// printf("%d\n", connect);
 				client.addNewClient(connect, events[i].ident);
 				EV_SET((*srvs[index]).getKevent(), connect, EVFILT_READ, EV_ADD, 0, 0, NULL);
 				kevent(kQueue, (*srvs[index]).getKevent(), 1, NULL, 0, NULL);
 			}
 			else if (events[i].filter == EVFILT_READ) {
-				// std::cout << RED << "Not found server" << RESET << std::endl;
 				size_t totalBytesRead = 0;
 				int bytesRead = 0;
 				index = getRightSocketFd(srvs, client.getRightConnection(events[i].ident)->evIdent);
 				char buff[8192];
 				do {
-					// std::cout << "[DEBUG 1]" << std::endl;
 					bytesRead = recv(events[i].ident, buff, 8192, 0);
-					// std::cout << "[DEBUG 2]" << std::endl;
 					totalBytesRead += bytesRead;
-					// std::cout << "[DEBUG 3]" << std::endl;
 					if (bytesRead > 0) {
 						bufferStr.append(buff, bytesRead);
-						// std::cout << "[DEBUG 4]" << std::endl;
 					}
 					usleep(100);
-					// std::cout << "[DEBUG 5]" << std::endl;
 				} while (bytesRead > 0);
 				req.parsereq(bufferStr);
 				std::cout << GREEN << "[DEBUG] " << index << RESET << std::endl;
@@ -109,7 +100,6 @@ int	main(int ac, char *av[]) {
 				std::cout << "cacca2" << std::endl;
 				req.setResponse(srvs[index], events[i].ident);
 				client.closeClientConnection(events[i].ident);
-				// memset(bufferino, 0, 10000);
 				close(events[i].ident);
 			}
 		}
